e56f3bc78dff168c277f9c6884db84b04256f080
Rithikeshh <rithikeshh@gmail.com>
2023-10-12 01:25:27 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,19 @@ export default function Home() {
     const [items, setItems] = useState([]);
     const [message, setMessage] = useState('');
 
-    
+    const addItemFun = useCallback(function handleItems(){
+        setItems((oldValue)=>{
+            return [...oldValue,`Item ${oldValue.length+1}`]
+        })
+        setMessage('Item added successfully!')
+        setTimeout(()=>{
+            setMessage('')
+        },3000)
+    },[])
 
     return (
         <div>
-            <button >Add Item</button>
+            <button onClick={addItemFun}>Add Item</button>
             <ItemList items={items} />
             {message && <p id='message'>{message}</p>}
         </div>
